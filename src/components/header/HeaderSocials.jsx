10966f064ff034aea1c2b194d7953880beb22334
Flavio Coscarella <flaviodc@live.com.ar>
2022-03-20 08:38:04 -0300

    Añadiendo Links redes Header

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import {BSLinkedin} from 'react-icons/bs'
+import {FaGithub} from 'react-icons/fa'
+import {SiCodewars} from 'react-icons/si'
+
 
 const HeaderSocials = () => {
   return (
     <div className="header_socials">
-        <a href="https://www.linkedin.com/in/flavio-coscarella/" target="_blank" rel="noopener noreferrer"></a>
-        
+        <a href="https://www.linkedin.com/in/flavio-coscarella/" target="_blank" rel="noopener noreferrer"><BSLinkedin/></a>
+        <a href="https://github.com/Flaviodc7" target="_blank" rel="noopener noreferrer"><FaGithub/></a>
+        <a href="https://www.codewars.com/users/Flaviodc7" target="_blank" rel="noopener noreferrer"><SiCodewars/></a>        
     </div>
   )
 }
