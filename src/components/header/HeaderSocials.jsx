ff60bd0001abf2bdb9f8a1830a91b9b378cd050c
Flavio Coscarella <flaviodc@live.com.ar>
2022-03-21 08:25:38 -0300

    Arreglando error del Header

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {BSLinkedin} from 'react-icons/bs'
+import {BsLinkedin} from 'react-icons/bs'
 import {FaGithub} from 'react-icons/fa'
 import {SiCodewars} from 'react-icons/si'
 
@@ -7,7 +7,7 @@ import {SiCodewars} from 'react-icons/si'
 const HeaderSocials = () => {
   return (
     <div className="header_socials">
-        <a href="https://www.linkedin.com/in/flavio-coscarella/" target="_blank" rel="noopener noreferrer"><BSLinkedin/></a>
+        <a href="https://www.linkedin.com/in/flavio-coscarella/" target="_blank" rel="noopener noreferrer"><BsLinkedin/></a>
         <a href="https://github.com/Flaviodc7" target="_blank" rel="noopener noreferrer"><FaGithub/></a>
         <a href="https://www.codewars.com/users/Flaviodc7" target="_blank" rel="noopener noreferrer"><SiCodewars/></a>        
     </div>
