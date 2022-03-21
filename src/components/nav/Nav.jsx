37ad9c7b92566bbd30e2048a1191eebda17adac1
Flavio Coscarella <flaviodc@live.com.ar>
2022-03-21 07:40:39 -0300

    Asignando iconos al nav

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import './nav.css'
+import {AiFillHome} from "react-icons/ai"
+import {FcAbout} from "react-icons/fc"
+import {GrProjects} from "react-icons/gr"
+import {BsPeopleFill} from "react-icons/bs"
+import {GrContact} from "react-icons/gr"
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <nav>
+      <a href="#"><AiFillHome /></a>
+      <a href="#about"><FcAbout /></a>
+      <a href="#projects"><GrProjects /></a>
+      <a href="#testimonials"><BsPeopleFill /></a>
+      <a href="#contact"><GrContact /></a>
+    </nav>
   )
 }
 
