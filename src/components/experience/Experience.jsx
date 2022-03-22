143b30469c092713ff342bdebcf1c9f3bd149700
Flavio Coscarella <flaviodc@live.com.ar>
2022-03-22 11:17:30 -0300

    Añadiendo contenido a Experiencia

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,9 +1,60 @@
 import React from 'react'
 import './experience.css'
+import {BsFillCheckCircleFill} from 'react-icons/bs'
 
 const Experience = () => {
   return (
-    <section id="experience">Experience</section>
+    <section id="experience">
+      <h5>Estos son mis skills</h5>
+      <h2>Mi experiencia</h2>
+      <div className="container experience__container">
+        <div className="experience__frontend">
+          <h3>Herramientas que utilizo</h3>
+          <div className="experience__content">
+            <article className="experience__details">
+              <BsFillCheckCircleFill />
+              <h4>HTML5</h4>
+              <small className="text-light">Experimentado</small>
+            </article>
+            <article className="experience__details">
+              <BsFillCheckCircleFill />
+              <h4>CSS3</h4>
+              <small className="text-light">Experimentado</small>
+            </article>
+            <article className="experience__details">
+              <BsFillCheckCircleFill />
+              <h4>JavaScript</h4>
+              <small className="text-light">Experimentado</small>
+            </article>
+            <article className="experience__details">
+              <BsFillCheckCircleFill />
+              <h4>ReactJS</h4>
+              <small className="text-light">Experimentado</small>
+            </article>
+            <article className="experience__details">
+              <BsFillCheckCircleFill />
+              <h4>BootStrap</h4>
+              <small className="text-light">Experimentado</small>
+            </article>
+            <article className="experience__details">
+              <BsFillCheckCircleFill />
+              <h4>Tailwind</h4>
+              <small className="text-light">Experimentado</small>
+            </article>
+            <article className="experience__details">
+              <BsFillCheckCircleFill />
+              <h4>GitHub</h4>
+              <small className="text-light">Experimentado</small>
+            </article>
+            <article className="experience__details">
+              <BsFillCheckCircleFill />
+              <h4>GIT</h4>
+              <small className="text-light">Experimentado</small>
+            </article>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
