8f81c7ac968c3a82ad7dc60dfb28435db9300790
Flavio Coscarella <flaviodc@live.com.ar>
2022-03-22 10:11:55 -0300

    Agregando contenido al About

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,10 +1,50 @@
-import React from 'react'
-import './about.css';
+import React from "react";
+import "./about.css";
+import me2about from "../../assets/me2-about.jpg";
+import { RiAwardFill } from "react-icons/ri";
+import { FiUsers } from "react-icons/fi";
+import { VscFolderActive } from "react-icons/vsc";
 
 const About = () => {
   return (
-    <section id="about">About</section>
-  )
-}
+    <section id="about">
+      <h5>Conoce</h5>
+      <h2>Acerca de mi</h2>
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={me2about} alt="Mi foto en acerca de" />
+          </div>
+        </div>
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <RiAwardFill className="about__icon" />
+              <h5>Experiencia</h5>
+              <small>1+ año de experiencia</small>
+            </article>
+            <article className="about__card">
+              <FiUsers className="about__icon" />
+              <h5>Clientes</h5>
+              <small>10+ Clientes</small>
+            </article>
+            <article className="about__card">
+              <VscFolderActive className="about__icon" />
+              <h5>Proyectps</h5>
+              <small>10+ Finalizados</small>
+            </article>
+          </div>
+          <p>
+            Soy un programador web dedicado y motivado en brindar
+            soluciones, aplicando lógica y buscando métodos resolutivos acordes.
+            Me gustan los desafíos, ya que implican un constante aprendizaje,
+            sea con un cambio de paradigma o una nueva tecnología
+          </p>
+          <a href="#contact" className="btn btn-primary">Contactame</a>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default About
+export default About;
