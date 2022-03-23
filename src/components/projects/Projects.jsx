47dc7c84c8e28e99b04ed678c6e100e3b00d79d0
Flavio Coscarella <flaviodc@live.com.ar>
2022-03-23 10:33:07 -0300

    Añadiendo estilos a Proyectos

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./projects.css";
-import IMG1 from "../../assets/porfolio1.jpg"
+import IMG1 from "../../assets/porfolio1.jpg";
 
 const Projects = () => {
   return (
@@ -8,27 +8,29 @@ const Projects = () => {
       <h5>Mi trabajo reciente</h5>
       <h2>Proyectos</h2>
       <div class="container projects__container">
-        <article className="project__item">
+        <article className="project__items">
           <div classname="project_item-image">
             <img src={IMG1} alt="Primer proyecto" />
           </div>
           <h3>Este es un articulo del portafolio </h3>
-          <a
-            href="http://github.com/fladiodc7"
-            className="btn"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Github
-          </a>
-          <a
-            href="http://github.com/fladiodc7"
-            className="btn btn-primary"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Demo
-          </a>
+          <div className="project__item-cta">
+            <a
+              href="http://github.com/fladiodc7"
+              className="btn"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Github
+            </a>
+            <a
+              href="http://github.com/fladiodc7"
+              className="btn btn-primary"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Demo
+            </a>
+          </div>
         </article>
       </div>
     </section>
