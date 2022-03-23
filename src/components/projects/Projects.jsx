38a0569efe8cfc54c990988b31d6a6a954bfe735
Flavio Coscarella <flaviodc@live.com.ar>
2022-03-23 10:16:59 -0300

    Añadiendo contenido a Proyectos

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,10 +1,38 @@
-import React from 'react'
-import './projects.css'
+import React from "react";
+import "./projects.css";
+import IMG1 from "../../assets/porfolio1.jpg"
 
 const Projects = () => {
   return (
-    <section id="projects">Projects</section>
-  )
-}
+    <section id="projects">
+      <h5>Mi trabajo reciente</h5>
+      <h2>Proyectos</h2>
+      <div class="container projects__container">
+        <article className="project__item">
+          <div classname="project_item-image">
+            <img src={IMG1} alt="Primer proyecto" />
+          </div>
+          <h3>Este es un articulo del portafolio </h3>
+          <a
+            href="http://github.com/fladiodc7"
+            className="btn"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Github
+          </a>
+          <a
+            href="http://github.com/fladiodc7"
+            className="btn btn-primary"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Demo
+          </a>
+        </article>
+      </div>
+    </section>
+  );
+};
 
-export default Projects
+export default Projects;
