7564d2d473a41257c98e478b88973f63b5d785a5
Flavio Coscarella <flaviodc@live.com.ar>
2022-03-23 09:56:15 -0300

    Añadiendo estilos a Experiencia

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import './experience.css'
-import {BsFillCheckCircleFill} from 'react-icons/bs'
+import React from "react";
+import "./experience.css";
+import { BsFillCheckCircleFill } from "react-icons/bs";
 
 const Experience = () => {
   return (
@@ -12,50 +12,66 @@ const Experience = () => {
           <h3>Herramientas que utilizo</h3>
           <div className="experience__content">
             <article className="experience__details">
-              <BsFillCheckCircleFill />
-              <h4>HTML5</h4>
-              <small className="text-light">Experimentado</small>
+              <BsFillCheckCircleFill className="experience__details-icon"/>
+              <div>
+                <h4>HTML5</h4>
+                <small className="text-light">Experimentado</small>
+              </div>
             </article>
             <article className="experience__details">
-              <BsFillCheckCircleFill />
-              <h4>CSS3</h4>
-              <small className="text-light">Experimentado</small>
+              <BsFillCheckCircleFill className="experience__details-icon"/>
+              <div>
+                <h4>CSS3</h4>
+                <small className="text-light">Experimentado</small>
+              </div>
             </article>
             <article className="experience__details">
-              <BsFillCheckCircleFill />
-              <h4>JavaScript</h4>
-              <small className="text-light">Experimentado</small>
+              <BsFillCheckCircleFill className="experience__details-icon"/>
+              <div>
+                <h4>JavaScript</h4>
+                <small className="text-light">Experimentado</small>
+              </div>
             </article>
             <article className="experience__details">
-              <BsFillCheckCircleFill />
-              <h4>ReactJS</h4>
-              <small className="text-light">Experimentado</small>
+              <BsFillCheckCircleFill className="experience__details-icon"/>
+              <div>
+                <h4>ReactJS</h4>
+                <small className="text-light">Experimentado</small>
+              </div>
             </article>
             <article className="experience__details">
-              <BsFillCheckCircleFill />
-              <h4>BootStrap</h4>
-              <small className="text-light">Experimentado</small>
+              <BsFillCheckCircleFill className="experience__details-icon"/>
+              <div>
+                <h4>BootStrap</h4>
+                <small className="text-light">Experimentado</small>
+              </div>
             </article>
             <article className="experience__details">
-              <BsFillCheckCircleFill />
-              <h4>Tailwind</h4>
-              <small className="text-light">Experimentado</small>
+              <BsFillCheckCircleFill className="experience__details-icon"/>
+              <div>
+                <h4>Tailwind</h4>
+                <small className="text-light">Experimentado</small>
+              </div>
             </article>
             <article className="experience__details">
-              <BsFillCheckCircleFill />
-              <h4>GitHub</h4>
-              <small className="text-light">Experimentado</small>
+              <BsFillCheckCircleFill className="experience__details-icon"/>
+              <div>
+                <h4>GitHub</h4>
+                <small className="text-light">Experimentado</small>
+              </div>
             </article>
             <article className="experience__details">
-              <BsFillCheckCircleFill />
-              <h4>GIT</h4>
-              <small className="text-light">Experimentado</small>
+              <BsFillCheckCircleFill className="experience__details-icon"/>
+              <div>
+                <h4>GIT</h4>
+                <small className="text-light">Experimentado</small>
+              </div>
             </article>
           </div>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Experience
+export default Experience;
