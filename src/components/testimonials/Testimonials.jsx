e812bac7942b005a874846a6e69fb46ba1bb3d04
Flavio Coscarella <flaviodc@live.com.ar>
2022-03-24 20:30:20 -0300

    Creando array con metodo map para Testimonios

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import './testimonials.css'
-import AVTR1 from '../../assets/avatar1.jpg'
+import React from "react";
+import "./testimonials.css";
+import { data } from "./testimonial-list";
 
 const Testimonials = () => {
   return (
@@ -9,18 +9,24 @@ const Testimonials = () => {
       <h2>Testimonios</h2>
 
       <div className="container testimonials__container">
-        <article className="testimonial">
-          <div className="client__avatar">
-            <img src={AVTR1} alt="Avatar 1" />
-          </div>
-          <h3 className="client__name">name</h3>
-            <small className="client__review">
-              review
-            </small>
-        </article>
+        {
+        data.map(
+          ({ avatar, name, review },
+          index) => {
+            return (
+            <article key={index} className="testimonial">
+              <div className="client__avatar">
+                <img src={avatar} alt="Avatar 1" />
+              </div>
+              <h3 className="client__name">{name}</h3>
+              <small className="client__review">{review}</small>
+            </article>
+            )
+          })
+        }
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Testimonials
+export default Testimonials;
---
+++ src/components/testimonials/testimonial-list.js
@@ -0,0 +1,9 @@
+import AVTR1 from '../../assets/avatar1.jpg'
+
+export const data = [
+    {
+        avatar: AVTR1,
+        name: "Nombre",
+        review: "Revisión"
+    }
+]
