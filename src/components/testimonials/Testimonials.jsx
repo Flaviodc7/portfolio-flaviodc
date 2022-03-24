7ea1b65849dfca6f9d92b61f41e5b55e3f872ec5
Flavio Coscarella <flaviodc@live.com.ar>
2022-03-24 18:15:46 -0300

    Añadiendo contenido a Testimonios

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 import './testimonials.css'
+import AVTR1 from '../../assets/avatar1.jpg'
 
 const Testimonials = () => {
   return (
-    <section id="testimonials">Testimonials</section>
+    <section id="testimonials">
+      <h3>Reseñas de Clientes</h3>
+      <h2>Testimonios</h2>
+
+      <div className="container testimonials__container">
+        <article className="testimonial">
+          <div className="client__avatar">
+            <img src={AVTR1} alt="Avatar 1" />
+            <h3 className="client__name">name</h3>
+            <small className="client__review">
+              review
+            </small>
+          </div>
+        </article>
+      </div>
+    </section>
   )
 }
 
