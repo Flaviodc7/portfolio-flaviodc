77d71b4a11e9ca6cc09fc8d5d3dd0a95907b2871
Flavio Coscarella <flaviodc@live.com.ar>
2022-03-25 10:12:34 -0300

    Añadiendo SwiperJS a Testimonios

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -2,29 +2,43 @@ import React from "react";
 import "./testimonials.css";
 import { data } from "./testimonial-list";
 
+// import Swiper core and required modules
+import { Pagination } from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
 const Testimonials = () => {
   return (
     <section id="testimonials">
       <h3>Reseñas de Clientes</h3>
       <h2>Testimonios</h2>
 
-      <div className="container testimonials__container">
+      <Swiper className="container testimonials__container"
+            // install Swiper modules
+            modules={[Pagination]}
+            spaceBetween={40}
+            slidesPerView={1}
+            pagination={{ clickable: true }}>
         {
         data.map(
           ({ avatar, name, review },
           index) => {
             return (
-            <article key={index} className="testimonial">
+            <SwiperSlide key={index} className="testimonial">
               <div className="client__avatar">
                 <img src={avatar} alt="Avatar 1" />
               </div>
               <h3 className="client__name">{name}</h3>
               <small className="client__review">{review}</small>
-            </article>
+            </SwiperSlide >
             )
           })
-        }
-      </div>
+         }
+      </Swiper>
     </section>
   );
 };
