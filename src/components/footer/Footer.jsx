295d168478b88e2cae405b2401013b379bdb68a4
Flavio Coscarella <flaviodc@live.com.ar>
2022-03-26 08:38:51 -0300

    Añandiendo contenido al footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,7 +3,25 @@ import './footer.css'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href="#" className='footer__logo'>Flavio Coscarella</a>
+
+      <ul className="permalinks">
+        <li><a href="#">Inicio</a></li>
+        <li><a href="#About">Acerca de</a></li>
+        <li><a href="#About">Experiencia</a></li>
+        <li><a href="#About">Proyectos</a></li>
+        <li><a href="#About">Testimonios</a></li>
+        <li><a href="#About">Contacto</a></li>
+      </ul>
+      <div className="footer__socials">
+        <a href="https://linkedin"></a>
+        <a href="https://Codewars"></a>
+      </div>
+      <div className="footer__copyright">
+        <small>&copy; Flavio Coscarella 2022. Todos los derechos reservador</small>
+      </div>
+    </footer>
   )
 }
 
