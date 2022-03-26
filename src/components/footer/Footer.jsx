8b26a0d9ff9421f5903364f40866b5dc100f9a27
Flavio Coscarella <flaviodc@live.com.ar>
2022-03-26 09:02:25 -0300

    Fix enlaces del Footer y añadiendo redes

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import './footer.css'
+import {BsLinkedin} from 'react-icons/bs'
+import {FaGithub} from 'react-icons/fa'
+import {SiCodewars} from 'react-icons/si'
 
 const Footer = () => {
   return (
@@ -8,18 +11,19 @@ const Footer = () => {
 
       <ul className="permalinks">
         <li><a href="#">Inicio</a></li>
-        <li><a href="#About">Acerca de</a></li>
-        <li><a href="#About">Experiencia</a></li>
-        <li><a href="#About">Proyectos</a></li>
-        <li><a href="#About">Testimonios</a></li>
-        <li><a href="#About">Contacto</a></li>
+        <li><a href="#about">Acerca de</a></li>
+        <li><a href="#experience">Experiencia</a></li>
+        <li><a href="#projects">Proyectos</a></li>
+        <li><a href="#testimonials">Testimonios</a></li>
+        <li><a href="#contact">Contacto</a></li>
       </ul>
       <div className="footer__socials">
-        <a href="https://linkedin"></a>
-        <a href="https://Codewars"></a>
+        <a href="https://www.linkedin.com/in/flavio-coscarella/" target="_blank" rel="noopener noreferrer"><BsLinkedin/></a>
+        <a href="https://github.com/Flaviodc7" target="_blank" rel="noopener noreferrer"><FaGithub/></a>
+        <a href="https://www.codewars.com/users/Flaviodc7" target="_blank" rel="noopener noreferrer"><SiCodewars/></a>
       </div>
       <div className="footer__copyright">
-        <small>&copy; Flavio Coscarella 2022. Todos los derechos reservador</small>
+        <small>&copy; Flavio Coscarella 2022. Todos los derechos reservados</small>
       </div>
     </footer>
   )
