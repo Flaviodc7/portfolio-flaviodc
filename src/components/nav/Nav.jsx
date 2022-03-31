477384ebfbd8fe0ad5f4824fa5480f2ff958a987
Flavio Coscarella <flaviodc@live.com.ar>
2022-03-31 09:21:43 -0300

    Modificando estilos del about y Nav

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useState } from 'react'
 import './nav.css'
 import {AiFillHome} from "react-icons/ai"
+import {RiAwardFill} from "react-icons/ri"
 import {AiOutlineUser} from "react-icons/ai"
 import {BiBookAlt} from "react-icons/bi"
 import {BsPeopleFill} from "react-icons/bs"
@@ -13,6 +14,7 @@ const Nav = () => {
     <nav>
       <a href="#" onClick={() => setActiveNav("#")}  className={activeNav === "#" ? "active" : ""}><AiFillHome /></a>
       <a href="#about" onClick={() => setActiveNav("#about")} className={activeNav === "#about" ? "active" : ""}><AiOutlineUser /></a>
+      <a href="#experience" onClick={() => setActiveNav("#experience")} className={activeNav === "#experience" ? "active" : ""}><RiAwardFill /></a>
       <a href="#projects" onClick={() => setActiveNav("#projects")} className={activeNav === "#projects" ? "active" : ""}><BiBookAlt /></a>
       <a href="#testimonials" onClick={() => setActiveNav("#testimonials")} className={activeNav === "#testimonials" ? "active" : ""}><BsPeopleFill /></a>
       <a href="#contact" onClick={() => setActiveNav("#contact")} className={activeNav === "#contact" ? "active" : ""}><FiMessageCircle /></a>
