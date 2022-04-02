9d10f642c183caf1450418fac2f9e9a917fc7d88
Flavio Coscarella <flaviodc@live.com.ar>
2022-04-02 07:35:40 -0300

    Añadiendo lista de Proyectos

--- src/components/projects/project-list.js
+++ src/components/projects/project-list.js
@@ -1,11 +1,18 @@
-import IMG1 from "../../assets/projects/portfolio1.jpg";
+import IMG1 from "../../assets/projects/project01.jpg";
 
 export const data = [
     {
         id: 1,
         image: IMG1,
-        title: "Primer proyecto",
-        github: "https://github.com/flaviodc7",
-        demo: "https://github.com/flaviodc7"
+        title: "Rick & Morty API",
+        github: "https://github.com/Flaviodc7/Rick-Morty",
+        demo: "https://rickmortyflavio.netlify.app/",
+    },
+    {
+        id: 2,
+        image: "",
+        title: "React Calculator",
+        github: "https://github.com/Flaviodc7/React-Calculator",
+        demo: "https://coscarella-calculator.netlify.app/",
     }
 ]
