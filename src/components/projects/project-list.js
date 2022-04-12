27e45ff3a2ed0092c5d13290fa7de2a2a66d366c
Flavio Coscarella <flaviodc@live.com.ar>
2022-04-12 11:37:42 -0300

    Actualizando Listado de Proyectos

--- src/components/projects/project-list.js
+++ src/components/projects/project-list.js
@@ -1,27 +1,36 @@
 import projectImg1 from "../../assets/projects/project01.jpg";
 import projectImg2 from "../../assets/projects/project02.jpg";
 import projectImg3 from "../../assets/projects/project03.jpg";
+import projectImg4 from "../../assets/projects/project04.jpg";
 
 export const data = [
     {
         id: 1,
         image: projectImg1,
+        title: "Portfolio",
+        description: "Portfolio personal creado con HTML, CSS y JavaScript con librerias React",
+        github: "https://github.com/Flaviodc7/portfolio-flaviodc",
+        demo: "https://coscarella.netlify.app/",
+    },
+    {
+        id: 2,
+        image: projectImg2,
         title: "Rick & Morty API",
         description: "App de Rick & Morty que muestra los personajes creada con React",
         github: "https://github.com/Flaviodc7/Rick-Morty",
         demo: "https://rickmortyflavio.netlify.app/",
     },
     {
-        id: 2,
-        image: projectImg2,
+        id: 3,
+        image: projectImg3,
         title: "React Calculator",
         description: "Calculadora creada con React y Styled Components",
         github: "https://github.com/Flaviodc7/React-Calculator",
         demo: "https://coscarella-calculator.netlify.app/",
     },
     {
-        id: 3,
-        image: projectImg3,
+        id: 4,
+        image: projectImg4,
         title: "Gallery App",
         description: "Galeria de Imagenes en React que usa la API de Unsplash",
         github: "https://github.com/Flaviodc7/Gallery-app",
